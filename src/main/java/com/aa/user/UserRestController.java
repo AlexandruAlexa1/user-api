eb8bdd6e69554fcd8e3f018ffef6c187c544b036
Alexandru Alexa <x.alx@yahoo.com>
2022-09-06 15:01:59 +0200

    Code update User

--- src/main/java/com/aa/user/UserRestController.java
+++ src/main/java/com/aa/user/UserRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,11 @@ public class UserRestController {
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
 		
 		return ResponseEntity.created(location).body(savedUser);
+	}
+	
+	@PutMapping
+	public ResponseEntity<User> update(@RequestBody User user) {
+		User updatedAccount = service.save(user);
+		return new ResponseEntity<>(updatedAccount, HttpStatus.OK);
 	}
 }
