e1b42053d8418018ddb38cbfd9d0a5043afcd695
Alexandru Alexa <x.alx@yahoo.com>
2022-09-06 12:43:45 +0200

    Code User Entity

---
+++ src/main/java/com/aa/user/User.java
@@ -0,0 +1,85 @@
+package com.aa.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(length = 125, unique = true)
+	private String email;
+	
+	@Column(length = 64, nullable = false)
+	private String password;
+	
+	@Column(name = "first_name", length = 45, nullable = false)
+	private String firstName;
+	
+	@Column(name = "last_name", length = 45, nullable = false)
+	private String lastName;
+	
+	private boolean enabled;
+	
+	public User() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+}
